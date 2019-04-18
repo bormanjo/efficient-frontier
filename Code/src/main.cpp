134541f54dcc51b4f48ac7d328fb4bb1bd69cb1f
J-C Borman <borman.johncraig@gmail.com>
2019-04-18 10:18:11 -0400

    Added the following tests: > Matrix inversion > Stock Iteration > Portfolio Construction

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -2,17 +2,86 @@
 #include <curl/curl.h>
 #include <regex>
 #include "YFData.h"
+#include "Eigen/Dense"
 
-int main()
-{
+void test_yf_data(){
     YFData yf;
 
     Stock aapl = yf.get_stock("AAPL", 1262304000, 1514764800);
 
-    cout << "AAPL's Adjusted close data" << endl;
+    cout << aapl.get_ticker() << "'s Adjusted Close Data" << endl;
 
     for (auto const& value:aapl.data) {
         cout << value << endl;
+    }
+}
+
+void test_eigen_matrix(){
+    Eigen::MatrixXd m(2,2);
+    m << 3, 2,    2.5, 1;
+
+    cout << m << endl;
+
+    cout << "Determinant: " << m.determinant() << endl;
+    cout << "Inverse: " << endl << m.inverse() << endl;
+}
+
+void test_stocks(){
+    long start = 1262304000, end = 1514764800;
+
+    YFData yf;
+    Stock aapl = yf.get_stock("AAPL", start, end);
+    Stock goog = yf.get_stock("GOOG", start, end);
+    Stock msft = yf.get_stock("MSFT", start, end);
+
+    vector<Stock> stocks({aapl, goog, msft});
+
+    for(unsigned int j= 0; j < 5; ++j){
+        for(const auto stock:stocks){
+            cout << stock.data[j] << "\t";
+        }
+        cout << endl;
+    }
+
+    cout << "AAPL Ret: " << aapl.get_expected_return() << endl;
+    cout << "GOOG Ret: " << goog.get_expected_return() << endl;
+    cout << "MSFT Ret: " << msft.get_expected_return() << endl;
+
+    for (auto stock:stocks) {
+        cout << stock.get_ticker() << " Var: " << stock.get_variance() << endl;
+    }
+
+}
+
+void test_portfolio(){
+    long start = 1262304000, end = 1514764800;
+
+    YFData yf;
+    Stock aapl = yf.get_stock("AAPL", start, end);
+    Stock goog = yf.get_stock("GOOG", start, end);
+    Stock msft = yf.get_stock("MSFT", start, end);
+
+    vector<Stock> stocks = {aapl, goog, msft};
+
+    cout << "Making portfolio..." << endl;
+
+    Portfolio port(stocks);
+
+    for (unsigned int i = 0; i < stocks.size(); ++i) {
+        cout << endl;
+        for (unsigned int j = 0; j < stocks.size(); ++j) {
+            cout << port.get_covar_matrix(i, j) << "\t";
+        }
+
+    }
+}
+
+int main()
+{
+    try{
+        test_portfolio();
+    } catch (exception &e){
+        cout << "Error: " << e.what() << endl;
     }
 
     return 0;
